568631d7cfe1349127d15d9c2784d6ca32dd78fb
BankZKuma <69504169+l3ankZKuma@users.noreply.github.com>
2021-04-19 19:19:56 +0700

    Add files via upload

---
+++ Beautiful Matrix.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int arr[5][5],cnt=0;
+	for(int i=0;i<5;++i){
+		for(int j=0;j<5;++j){
+			cin>>arr[i][j];
+			if(arr[i][j]==1){
+				cnt=abs(i-2)+abs(j-2);
+				break;
+			}
+		}
+	}
+	cout<<cnt;
+}
