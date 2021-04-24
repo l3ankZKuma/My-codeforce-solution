928a534ab177f73b15fb6331c1724ff0d62ffeec
BankZKuma <69504169+l3ankZKuma@users.noreply.github.com>
2021-04-24 11:38:47 +0700

    Update IQ test.cpp

--- IQ test.cpp
+++ IQ test.cpp
@@ -3,6 +3,25 @@ using namespace std;
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+	int n,cntE=0,cntO=0,indexO,indexE;
 	cin>>n;
-	
+	vector<int> nums(n);
+	for(int i=0;i<n;++i){
+		cin>>nums[i];
+		if(nums[i]%2==0){
+			++cntE;
+			indexE=i+1;
+		}
+		else{
+			indexO=i+1;
+			++cntO;
+		}
+	}
+	if(cntE==1){
+		cout<<indexE;
+	}
+	else{
+		cout<<indexO;
+	}
+
 }
