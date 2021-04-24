819b72637ffc9bbc2dbf8c95e0a6b94811b1e177
BankZKuma <69504169+l3ankZKuma@users.noreply.github.com>
2021-04-24 13:01:56 +0700

    Add files via upload

---
+++ Ichihime and Triangle.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+void slove(){
+	int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	printf("%d %d %d\n",b,c,c);
+}
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int T;
+	cin>>T;
+	while(T--){
+		slove();
+	}	
+}	
